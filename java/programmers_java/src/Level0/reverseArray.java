de0484207b5860800cfb0e6bf22559c28d09d486
Yeeun Kim <maemae2@naver.com>
2022-10-26 01:17:40 +0900

    레벨0 - 배열 뒤집기

---
+++ java/programmers_java/src/Level0/reverseArray.java
@@ -0,0 +1,17 @@
+package Level0;
+
+import java.util.Arrays;
+
+public class reverseArray {
+    public static int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for(int i=0 ; i<answer.length; i++) {
+            answer[i] = num_list[num_list.length - i-1];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[] {1, 2, 3, 4, 5})));
+    }
+}
