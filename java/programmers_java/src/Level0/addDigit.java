60ae5eb9d51553ad8aebdeb22e7fb392ed6765ed
Yeeun Kim <maemae2@naver.com>
2022-10-26 01:43:39 +0900

    레벨0 - 자릿수 더하기

---
+++ java/programmers_java/src/Level0/addDigit.java
@@ -0,0 +1,18 @@
+package Level0;
+
+public class addDigit {
+    public static int solution(int n) {
+        //String str = n+"";
+        String str = String.valueOf(n);
+        String[] strArr = str.split("");
+        int sum = 0;
+        for(String string : strArr) {
+            sum += Integer.parseInt(string);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(930211));
+    }
+}
