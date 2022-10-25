b6eb40e4dd7fe26d88d9182e4f39db8d6bc873aa
Yeeun Kim <maemae2@naver.com>
2022-10-26 02:02:52 +0900

    레벨0 - 배열 자르기

--- java/programmers_java/src/Level0/cuttingArray.java
+++ java/programmers_java/src/Level0/cuttingArray.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class cuttingArray {
     public static int[] solution(int[] numbers, int num1, int num2) {
         int[] answer = new int[num2-num1+1];
-        for(int i=0 ; i<answer.length; i++) {
-            answer[i] = numbers[num1+i];
-        }
+        System.arraycopy(numbers, num1, answer, 0, answer.length);
+//        for(int i=0 ; i<answer.length; i++) {
+//            answer[i] = numbers[num1+i];
+//        }
         return answer;
     }
 
