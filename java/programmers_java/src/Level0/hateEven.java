9c0d416919e87cdf2076dc978e2993bae5dc5454
Yeeun Kim <maemae2@naver.com>
2022-10-26 01:58:11 +0900

    레벨0 - 짝수는 싫어요

---
+++ java/programmers_java/src/Level0/hateEven.java
@@ -0,0 +1,17 @@
+package Level0;
+
+import java.util.Arrays;
+
+public class hateEven {
+    public static int[] solution(int n) {
+        int[] answer = new int[n%2==0? n/2 : n/2+1];
+        for(int i=0 ; i<answer.length; i++) {
+            answer[i] = i*2 + 1 ;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(15)));
+    }
+}
