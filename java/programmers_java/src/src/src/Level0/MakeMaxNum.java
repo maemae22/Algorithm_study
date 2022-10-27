febfb4c95d355d5f8f01e815bfbf5af059233fb9
Yeeun Kim <maemae2@naver.com>
2022-10-27 13:46:27 +0900

    레벨0 - 최댓값 만들기 (1)

---
+++ java/programmers_java/src/src/src/Level0/MakeMaxNum.java
@@ -0,0 +1,16 @@
+package src.src.Level0;
+
+import java.util.Arrays;
+
+public class MakeMaxNum {
+    public static int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1]*numbers[numbers.length-2];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {1, 7, 8}));
+    }
+
+
+}
