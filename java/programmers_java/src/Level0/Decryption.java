6dce254e35776dae5fc8724767b494b912094aa8
Yeeun Kim <maemae2@naver.com>
2022-10-28 20:53:43 +0900

    레벨0 - 암호 해독

---
+++ java/programmers_java/src/Level0/Decryption.java
@@ -0,0 +1,28 @@
+package Level0;
+
+public class Decryption {
+    public String solution(String cipher, int code) {
+        // 방법1 : 이게 더 빠름
+        /*
+        String[] str = cipher.split("");
+        String[] answerArr = new String[cipher.length()/code];
+        int num = 0;
+        for(int i=1; i<=str.length; i++) {
+            if(i%code==0) {
+                answerArr[num] = str[i-1];
+                num++;
+            }
+        }
+        return String.join("", answerArr);
+        */
+
+        // 방법 2 : 느림
+        String answer = "";
+        for(int i=1; i<=cipher.length(); i++) {
+            if(i%code==0) {
+                answer+= cipher.charAt(i-1);
+            }
+        }
+        return answer;
+    }
+}
