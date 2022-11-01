e9fe4890e1192287e535fdc3ce5aec8d64a4a79e
Yeeun Kim <maemae2@naver.com>
2022-11-01 15:29:45 +0900

    레벨1 - x만큼 간격이 있는 n개의 숫자

---
+++ java/programmers_java/src/Level1/NumbersSpacedByX.java
@@ -0,0 +1,13 @@
+package Level1;
+
+public class NumbersSpacedByX {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        answer[0] = x;
+        for(int i=1; i<answer.length; i++) {
+            answer[i] = answer[i-1] + x;
+        }
+        return answer;
+    }
+}
