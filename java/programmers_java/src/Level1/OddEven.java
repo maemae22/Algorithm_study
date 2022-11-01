1e3e56e6e643bbe5e3108f934e91d30c9b16e747
Yeeun Kim <maemae2@naver.com>
2022-11-01 14:57:29 +0900

    레벨1 - 짝수와 홀수

---
+++ java/programmers_java/src/Level1/OddEven.java
@@ -0,0 +1,16 @@
+package Level1;
+
+public class OddEven {
+    public String solution(int num) {
+        /*
+        if (num%2==0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+        */
+
+        return num%2==0? "Even" : "Odd";
+
+    }
+}
