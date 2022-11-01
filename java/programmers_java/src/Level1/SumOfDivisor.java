8983c2e19ac172a107bde236936fb6d57d262c1a
Yeeun Kim <maemae2@naver.com>
2022-11-01 14:45:01 +0900

    레벨1 - 약수의 합

---
+++ java/programmers_java/src/Level1/SumOfDivisor.java
@@ -0,0 +1,13 @@
+package Level1;
+
+public class SumOfDivisor {
+    public int solution(int n) {
+        int sum = 0;
+        for(int i=1; i<=n; i++) {
+            if(n%i == 0) {
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
