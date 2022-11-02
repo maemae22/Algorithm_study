6b9769f850c9917e45072fd77474367dcca020fd
Yeeun Kim <maemae2@naver.com>
2022-11-02 16:25:40 +0900

    레벨0 - 진료 순서 정하기

---
+++ java/programmers_java/src/Level0/SettingOrder.java
@@ -0,0 +1,25 @@
+package Level0;
+
+import java.util.*;
+
+public class SettingOrder {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+
+        //int[] copy = Arrays.copyOf(emergency, emergency.length);
+        //int[] copy = emergency.clone();
+        List<Integer> copy = new ArrayList<>();
+        for(int tmp : emergency) {
+            copy.add(tmp);
+        }
+        Collections.sort(copy, Collections.reverseOrder());
+
+        for(int i=0; i<emergency.length; i++) {
+            answer[i] = copy.indexOf(emergency[i]) + 1;
+        }
+
+
+        return answer;
+
+    }
+}
