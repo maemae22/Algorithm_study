a4dd703b7c3e1e5d06b5c9f10850523af9ad23e5
Yeeun Kim <maemae2@naver.com>
2022-11-07 21:12:01 +0900

    레벨2 - 올바른 괄호

---
+++ java/programmers_java/src/Level2/CorrectBracket.java
@@ -0,0 +1,35 @@
+package Level2;
+
+public class CorrectBracket {
+    boolean solution(String s) {
+        /*
+        String[] str = s.split("");
+        int open = 0;
+        int close = 0;
+        if(str[0].equals(")")) return false;
+        for(String tmp : str) {
+            if(tmp.equals("(")) {
+                open++;
+            } else {
+                close++;
+            }
+            if(open < close) return false;
+        }
+
+        return open == close ;
+        */
+
+        if(s.charAt(0) == ')') return false;
+        int open = 0;
+        int close = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') open ++;
+            else close++;
+            if(open < close) return false;
+        }
+
+        return open == close ;
+
+    }
+}
