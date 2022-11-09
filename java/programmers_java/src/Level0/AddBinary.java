762d76753dee647a5fea2f82a087db9009645c87
Yeeun Kim <maemae2@naver.com>
2022-11-10 01:18:17 +0900

    레벨0 - 이진수 더하기 (풀이 추가)

--- java/programmers_java/src/Level0/AddBinary.java
+++ java/programmers_java/src/Level0/AddBinary.java
@@ -9,4 +9,8 @@ public class AddBinary {
 
         return Integer.toBinaryString(sum);
     }
+
+    public String solution2(String bin1, String bin2) {
+        return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2),2);
+    }
 }
