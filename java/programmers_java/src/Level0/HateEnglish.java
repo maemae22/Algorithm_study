16258463dc8a12797067d983bd96babdc5c97d09
Yeeun Kim <maemae2@naver.com>
2022-11-10 02:34:35 +0900

    레벨0 - 영어가 싫어요 (풀이 추가)

--- java/programmers_java/src/Level0/HateEnglish.java
+++ java/programmers_java/src/Level0/HateEnglish.java
@@ -18,6 +18,47 @@ public class HateEnglish {
         return Long.parseLong(number9);
     }
 
+    public long solution2(String numbers) {
+        String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < numbers_arr.length; i++) {
+            numbers = numbers.replace(numbers_arr[i], String.valueOf(i));
+        }
+
+        long answer = Long.parseLong(numbers);
+        return answer;
+    }
+
+    public long solution3(String numbers) {
+        return Long.parseLong(
+                numbers.replace("zero", "0")
+                        .replace("one", "1")
+                        .replace("two", "2")
+                        .replace("three", "3")
+                        .replace("four", "4")
+                        .replace("five", "5")
+                        .replace("six", "6")
+                        .replace("seven", "7")
+                        .replace("eight", "8")
+                        .replace("nine", "9")
+        );
+    }
+
+    public long solution4(String numbers) {
+        numbers = numbers.replace("zero", "0");
+        numbers = numbers.replace("one", "1");
+        numbers = numbers.replace("two", "2");
+        numbers = numbers.replace("three", "3");
+        numbers = numbers.replace("four", "4");
+        numbers = numbers.replace("five", "5");
+        numbers = numbers.replace("six", "6");
+        numbers = numbers.replace("seven", "7");
+        numbers = numbers.replace("eight", "8");
+        numbers = numbers.replace("nine", "9");
+
+        return Long.parseLong(numbers);
+    }
+
     public static void main(String[] args) {
         System.out.println(solution("one"));
         System.out.println(solution("onetwothreefourfivesixseveneightnine"));
