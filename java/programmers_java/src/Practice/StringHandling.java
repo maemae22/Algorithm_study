0dbffa08e1aeedbf21edd9dec45f6e1750e7dd18
Yeeun Kim <maemae2@naver.com>
2022-11-18 01:41:43 +0900

    연습문제 - 문자열 다루기 기본

---
+++ java/programmers_java/src/Practice/StringHandling.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class StringHandling {
+    public boolean solution(String s) {
+        if(!(s.length()==4 || s.length()==6)) {
+            return false;
+        }
+
+        s = s.replaceAll("[0-9]","");
+        if(s.equals("")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
