e4ca30c52fb28fb4082394169697fae854d57442
Yeeun Kim <maemae2@naver.com>
2022-11-19 00:45:14 +0900

    레벨1 - 음양 더하기 (풀이 추가)

--- java/programmers_java/src/Level1/AdditionOfYinAndYang.java
+++ java/programmers_java/src/Level1/AdditionOfYinAndYang.java
@@ -20,4 +20,33 @@ public class AdditionOfYinAndYang {
         }
         return answer;
     }
+
+    public int solution2(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i<signs.length; i++)
+            answer += absolutes[i] * (signs[i]? 1: -1);
+        return answer;
+    }
+
+    public int solution3(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += (signs[i]) ? absolutes[i] : -absolutes[i];
+        }
+        return answer;
+    }
+
+    public int solution4(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for(int i=0; i<absolutes.length; i++){
+            if(signs[i]){
+                answer += absolutes[i];
+            }else{
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
 }
