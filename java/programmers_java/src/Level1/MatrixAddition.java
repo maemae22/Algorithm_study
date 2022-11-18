9ae6625146a7494360fc632c3dcd32a276bd722d
Yeeun Kim <maemae2@naver.com>
2022-11-19 02:23:27 +0900

    레벨1 - 행렬의 덧셈

---
+++ java/programmers_java/src/Level1/MatrixAddition.java
@@ -0,0 +1,13 @@
+package Level1;
+
+public class MatrixAddition {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for(int i=0; i<arr1.length; i++) {
+            for(int k=0; k<arr1[0].length; k++) {
+                answer[i][k] = arr1[i][k] + arr2[i][k];
+            }
+        }
+        return answer;
+    }
+}
