af79f16100f8f57aa5a9ac62ebebd0feb91fcb32
maemae22 <maemae2@naver.com>
2023-03-31 10:52:48 +0900

    P03_최솟값 구하기 / https://school.programmers.co.kr/learn/courses/30/lessons/59038

---
+++ sql/programmers_sql/Lv2/P03_최솟값 구하기.sql
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/59038
+
+# 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+SELECT MIN(DATETIME) AS '시간' FROM ANIMAL_INS
+
+SELECT DATETIME AS '시간' FROM ANIMAL_INS ORDER BY DATETIME LIMIT 1
