271c52e88c6680845d9001887718bf06ea0ea39a
maemae22 <maemae2@naver.com>
2024-01-21 23:18:17 +0900

    S2930_힙 (풀이 추가) / https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV-Tj7ya3jYDFAXr

--- java/SWEA_java/24동계 SW알고리즘 역량강화 과정/S2930_힙.java
+++ java/SWEA_java/24동계 SW알고리즘 역량강화 과정/S2930_힙.java
@@ -27,4 +27,61 @@ public class S2930_힙 {
             System.out.println();
         }
     }
+
+    public static void main2(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int testCase=1; testCase<=T; testCase++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] q = new int[N+1];
+            int size = 0;
+            System.out.print("#"+testCase+" ");
+            for (int i=0; i<N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int query = Integer.parseInt(st.nextToken());
+                if (query==1) {
+                    int add = Integer.parseInt(st.nextToken());
+                    q[++size] = add;
+                    int child = size;
+                    int parent = child/2;
+                    while (parent!=0 && q[parent]<q[child]) {
+                        int tmp = q[parent];
+                        q[parent] = q[child];
+                        q[child] = tmp;
+
+                        child = parent;
+                        parent = child/2;
+                    }
+                } else if (query==2) {
+                    if (size==0) {
+                        System.out.print("-1 ");
+                    } else {
+                        System.out.print(q[1]+" ");
+                        q[1] = q[size--];
+                        int parent = 1;
+                        int large = parent;
+                        while (true) {
+                            int left = parent*2;
+                            int right = parent*2+1;
+                            if (left<=size && q[large]<q[left]) {
+                                large = left;
+                            }
+                            if (right<=size && q[large]<q[right]) {
+                                large = right;
+                            }
+                            if (parent==large) {
+                                break;
+                            } else {
+                                int tmp = q[parent];
+                                q[parent] = q[large];
+                                q[large] = tmp;
+                                parent = large;
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
 }
